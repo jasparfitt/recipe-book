f0797866219efe172e7c4c7ade2dafe93d7a7dd3
Jason Parfitt <jasparfitt@gmail.com>
2025-11-28 19:40:21 +0000

    feat: include sourcemap

--- web/vite.config.js
+++ web/vite.config.js
@@ -5,7 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build"
+    outDir: "build",
+    sourcemap: true,
   },
   base: "/recipe-book/",
   server: {
